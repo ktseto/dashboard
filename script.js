758e1d5bb9f16e266712236eb54f1e87405dd736
Kameron Seto <tan@Sad-Panda.local>
2019-02-17 16:34:06 -0800

    doodle basic functionalities done

--- script.js
+++ script.js
@@ -40,3 +40,37 @@ toDoList.addEventListener('click', (e) => {
   listItems[index].completed = !listItems[index].completed;
   localStorage.setItem('todolist', JSON.stringify(listItems));
 });
+
+// Doodle component
+const canvas = document.querySelector('canvas');
+canvas.height = window.innerHeight * 0.3;
+canvas.width = window.innerWidth * 0.5;
+
+const ctx = canvas.getContext('2d');
+let strokeHue = 0;
+ctx.lineCap = 'round';
+ctx.lineWidth = 30;
+
+let isDrawing = false;
+let lastX, lastY;
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mousemove', (e) => {
+  if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle = `hsl(${strokeHue}, 100%, 50%)`;
+  ctx.stroke();
+  strokeHue += 1;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+const clearCanvas = document.querySelector('.clearCanvas');
+clearCanvas.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+// 
