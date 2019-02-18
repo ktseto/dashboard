7934a1b25cd720781eb6aed2108c93a447563291
Kameron Seto <tan@Sad-Panda.local>
2019-02-17 16:04:23 -0800

    todo list basic functionalities done

--- script.js
+++ script.js
@@ -3,16 +3,40 @@ const brightness = document.querySelector('.brightnessContainer input');
 const initialBrightness = getComputedStyle(document.body).getPropertyValue('--brightness');
 brightness.value = parseInt(initialBrightness.slice(0, initialBrightness.length - 1));
 
-brightness.addEventListener('change', () => {
-  document.documentElement.style.setProperty('--brightness', `${brightness.value}%`);
+brightness.addEventListener('change', function() {
+  document.documentElement.style.setProperty('--brightness', `${this.value}%`);
 });
 
-const listItems = localStorage.getItem('todolist') || [];
-// item = { text: '', completed: true }
-
+// To-do list component
+const listItems = JSON.parse(localStorage.getItem('todolist')) || [];
 const toDoList = document.querySelector('.toDoList');
-const HTMLtext = listItems.map((item) => `
-  <li>
-    <input type="checkbox" ${item.completed ? 'checked' : ''}>${item.text}
-  </li>
-`);
+
+const populateList = () => {
+  const HTMLtext = listItems.map((item, i) => `
+    <li>
+      <input type="checkbox" data-index=${i} ${item.completed ? 'checked' : ''}>${item.text}
+    </li>
+  `).join('');
+  toDoList.innerHTML = HTMLtext;
+}
+populateList();
+
+const addItemForm = document.querySelector('.addItemForm');
+addItemForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const inputText = this.querySelector('[type="text"]').value;
+  listItems.push({
+    text: inputText,
+    completed: false,
+  });
+  populateList();
+  localStorage.setItem('todolist', JSON.stringify(listItems));
+  this.reset();
+});
+toDoList.addEventListener('click', (e) => {
+  if (!e.target.matches('input')) return;
+
+  const index = e.target.dataset.index;
+  listItems[index].completed = !listItems[index].completed;
+  localStorage.setItem('todolist', JSON.stringify(listItems));
+});
